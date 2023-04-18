c25a5cad8f2daf476ab918678db359ad1f463c1c
Marcelo <marcsopranzi@gmail.com>
2023-04-18 05:46:04 +0100

    remove public access

--- storage.tf
+++ storage.tf
@@ -27,21 +27,3 @@ resource "aws_s3_object" "folders-discovery" {
   for_each = toset(["csv-ingestion/"])
   key      = each.key
 }
-
-data "aws_iam_policy_document" "public_read_access" {
-  statement {
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-    actions = [
-      "s3:GetObject",
-      "s3:ListBucket",
-    ]
-    resources = [
-      aws_s3_bucket.bucket-data-raw.arn,
-      "${aws_s3_bucket.bucket-data-raw.arn}/*",
-    ]
-  }
-}
-
